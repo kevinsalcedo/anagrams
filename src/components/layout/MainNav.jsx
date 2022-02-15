b8619a22f867a37fb7c0d4ebbc714e047b8cf0d5
Kevin Salcedo <salcedo.i.kevin@gmail.com>
2022-02-15 16:09:37 -0600

    changed Home to Sevens, resized the settings icon, change navbar to slide-in sidebar

--- src/components/layout/MainNav.jsx
+++ src/components/layout/MainNav.jsx
@@ -1,53 +1,76 @@
+import { useState } from "react";
+
 function MainNav({ setGame }) {
+  const [collapsed, setCollapsed] = useState(true);
+
+  function toggleGame(game) {
+    setGame(game);
+    setCollapsed((prev) => true);
+  }
   return (
     <nav className='nav-fixed-top w-100 navbar navbar-expand-lg navbar-light bg-light'>
       <div className='container-fluid'>
         <button
-          className='navbar-toggler'
+          className={`navbar-toggler ${collapsed ? "collapsed" : ""}`}
           type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          data-bs-toggle='offcanvas'
+          data-bs-target='#offcanvasNavbar'
+          aria-controls='offcanvasNavbar'
         >
           <span className='navbar-toggler-icon'></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item'>
-              <button
-                className='btn nav-link'
-                onClick={() => setGame("sevens")}
-              >
-                Home
-              </button>
-            </li>
-            <li className='nav-item'>
-              <button
-                className='btn nav-link'
-                data-bs-target='#navbarSupportedContent'
-                aria-controls='navbarSupportedContent'
-                onClick={() => setGame("eights")}
-              >
-                Eights
-              </button>
-            </li>
-            <li className='nav-item'>
-              <button
-                className='btn nav-link'
-                data-bs-target='#navbarSupportedContent'
-                aria-controls='navbarSupportedContent'
-                onClick={() => setGame("archived-sevens")}
-              >
-                Archive
-              </button>
-            </li>
-          </ul>
+        <div
+          className='offcanvas offcanvas-end'
+          tabIndex='-1'
+          id='offcanvasNavbar'
+        >
+          <div className='offcanvas-header'>
+            <h5 className='offcanvas-title' id='offcanvasNavbarLabel'>
+              Anagram of the Day
+            </h5>
+            <button
+              type='button'
+              className='btn-close text-reset'
+              data-bs-dismiss='offcanvas'
+              aria-label='Close'
+            ></button>
+          </div>
+          <div className='offcanvas-body'>
+            <ul className='navbar-nav justify-content-end flex-grow-1 pe-3'>
+              <li className='nav-item '>
+                <button
+                  className='btn nav-link'
+                  onClick={() => toggleGame("sevens")}
+                >
+                  Sevens
+                </button>
+              </li>
+              <li className='nav-item'>
+                <button
+                  className='btn nav-link'
+                  data-bs-target='#navbarSupportedContent'
+                  aria-controls='navbarSupportedContent'
+                  onClick={() => toggleGame("eights")}
+                >
+                  Eights
+                </button>
+              </li>
+              <li className='nav-item'>
+                <button
+                  className='btn nav-link'
+                  data-bs-target='#navbarSupportedContent'
+                  aria-controls='navbarSupportedContent'
+                  onClick={() => toggleGame("archived-sevens")}
+                >
+                  Archive
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
         <button
           type='button'
-          className='btn btn-secondary'
+          className='btn btn-secondary d-flex justify-content-center'
           data-bs-toggle='modal'
           data-bs-target='#settingsModal'
         >
@@ -56,7 +79,7 @@ function MainNav({ setGame }) {
             width='16'
             height='16'
             fill='currentColor'
-            className='bi bi-gear-fill'
+            className='bi bi-gear-fill mx-auto my-auto'
             viewBox='0 0 16 16'
           >
             <path d='M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z' />
